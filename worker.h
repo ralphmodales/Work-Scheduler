f4ef2a9acb7de56611ec71d3ba1da0704c06e55b
ralphmodales <148464942+ralphmodales@users.noreply.github.com>
2024-05-22 18:44:40 +0800

    Create worker.h

---
+++ worker.h
@@ -0,0 +1,14 @@
+#ifndef WORKER_H
+#define WORKER_H
+
+#include <string>
+#include <vector>
+
+struct Worker{
+                int id;
+                        std::string name;
+                                std::vector<std::vector<int>> availability;
+                                        std::vector<std::string> knowledge;
+};
+
+#endif
